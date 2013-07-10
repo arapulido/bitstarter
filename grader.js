b56756a93f52275614a897be0d528bb85381b246
Ara Pulido <apulido@gmail.com>
2013-07-10 21:58:16 +0000

    Add new categories to parse

--- grader.js
+++ grader.js
@@ -21,6 +21,7 @@
  *                            - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
  *                            */
 
+var rest = require('restler');
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
@@ -29,32 +30,55 @@ var CHECKSFILE_DEFAULT = "checks.json";
 
 var assertFileExists = function(infile) {
         var instr = infile.toString();
-        console.log(instr);
-            if(!fs.existsSync(instr)) {
-                        console.log("%s does not exist. Exiting.", instr);
-                                process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
-                                    }
-                return instr;
+        if(!fs.existsSync(instr)) {
+            console.log("%s does not exist. Exiting.", instr);
+            process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+        }
+        return instr;
 };
 
-var cheerioHtmlFile = function(htmlfile) {
-        return cheerio.load(fs.readFileSync(htmlfile));
+
+var cheerioHtmlFile = function(htmlBuffer) {
+        return cheerio.load(htmlBuffer);
 };
 
 var loadChecks = function(checksfile) {
         return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-        $ = cheerioHtmlFile(htmlfile);
-            var checks = loadChecks(checksfile).sort();
-                var out = {};
-                    for(var ii in checks) {
-                                var present = $(checks[ii]).length > 0;
-                                        out[checks[ii]] = present;
-                                            }
-                        return out;
+var checkHtmlFile = function(htmlBuffer, checksfile) {
+    $ = cheerioHtmlFile(htmlBuffer);
+    var checks = loadChecks(checksfile).sort();
+    var out = {};
+    for(var ii in checks) {
+        var present = $(checks[ii]).length > 0;
+        out[checks[ii]] = present;
+    }
+
+   var outJson = JSON.stringify(out, null, 4); 
+   console.log(outJson);
 };
+
+var buildfn = function(checks) {
+    var parseURL = function(result, response) {
+    if (result instanceof Error) {
+        console.error('Error: ' + util.format(response.message));
+    } else {
+        checkHtmlFile(response.rawEncoded, checks);
+    }
+    };
+    return parseURL;
+};
+
+var parseURL = function(result, response) {
+    
+
+    if (result instanceof Error) {
+        console.error('Error');
+    } else {
+        checkHTMLFile(response, checks);
+    } 
+}
 
 var clone = function(fn) {
 // Workaround for commander.js issue.
@@ -65,9 +89,16 @@ if(require.main == module) {
     program 
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'URL to the HTML file to check')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4); console.log(outJson);
+      if (program.url) {
+       parseURL = buildfn(program.checks);
+       rest.get(program.url).on('complete', parseURL); 
+    }
+    else {
+        var buffer = fs.readFileSync(program.file);
+        checkHtmlFile(buffer, program.checks);
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
